bc9d77ece36877559d0d5f9cb09b7181d4c7e075
Reham <r_nour30@yahoo.com>
2024-05-25 06:43:20 -0700

    fix index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,10 +16,11 @@ def status_of_api():
 def stats():
     """endpoint that retrieves the number of each objects by type"""
     stats = {
-            'states': storage.count('State'),
-            'cities': storage.count('City'),
-            'amenities': storage.count('Amenity'),
-            'places': storage.count('Place'),
-            'reviews': storage.count('Review'),
+            "states": storage.count('State'),
+            "cities": storage.count('City'),
+            "amenities": storage.count('Amenity'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "users": storage.count('User')
             }
     return (jsonify(stats))
