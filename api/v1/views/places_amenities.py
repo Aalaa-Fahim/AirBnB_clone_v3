b69906a0c7ba8966e5e41dd7b48858d89040f00d
Aalaa-Fahim <alaahussin.322001@gmail.com>
2024-05-28 00:26:05 +0300

    updated method

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -17,17 +17,16 @@ def amenity_by_place(place_id):
     """
     get all amenities of a place
     Args place_id: amenity id
-    :return: all amenities
+    return: all amenities
     """
-    fetched_obj = storage.get(Place, str(place_id))
+    fetched_obj = storage.get(Place, place_id)
 
     all_amenities = []
 
     if fetched_obj is None:
         abort(404)
 
-    for obj in fetched_obj.amenities:
-        all_amenities.append(obj.to_json())
+    all_amenities = [amenity.to_dict() for amenity in place.all_amenities]
 
     return jsonify(all_amenities)
 
