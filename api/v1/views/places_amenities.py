0d55732c81e710e8660d681bcc77afeb3dfb35e1
Aalaa-Fahim <alaahussin.322001@gmail.com>
2024-05-28 00:58:46 +0300

    updated pycodestyle

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -11,8 +11,10 @@ from os import getenv
 
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
-@app_views.route('/places/<place_id>/amenities',
-                 methods=['GET'], strict_slashes=False)
+
+@app_views.route("/places/<place_id>/amenities",
+                 methods=["GET"],
+                 strict_slashes=False)
 def get_place_amenities(place_id):
     """
     Retrieves the list of all Amenity objects of a Place.
@@ -36,8 +38,10 @@ def get_place_amenities(place_id):
         ]
     return jsonify([amenity.to_dict() for amenity in amenities])
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE'], strict_slashes=False)
+
+@app_views.route("/places/<place_id>/amenities/<amenity_id>",
+                 methods=["DELETE"],
+                 strict_slashes=False)
 def delete_place_amenity(place_id, amenity_id):
     """
     Deletes an Amenity object from a Place.
@@ -71,8 +75,10 @@ def delete_place_amenity(place_id, amenity_id):
     storage.save()
     return jsonify({}), 200
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['POST'], strict_slashes=False)
+
+@app_views.route("/places/<place_id>/amenities/<amenity_id>",
+                 methods=["POST"],
+                 strict_slashes=False)
 def post_place_amenity(place_id, amenity_id):
     """
     Links an Amenity object to a Place.
