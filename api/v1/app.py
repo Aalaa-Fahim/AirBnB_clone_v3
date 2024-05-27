37c231b5052610f34a45a90c160415a00d7e1ed7
Aalaa-Fahim <143665741+Aalaa-Fahim@users.noreply.github.com>
2024-05-27 22:40:24 +0300

    Creating CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,14 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
